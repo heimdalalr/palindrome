d57e749796e602189459d0583ad7c32110d9ac41
heimdalalr <lamorliere@gmail.com>
2021-10-22 18:24:52 -0400

    palindrome output correct

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,17 @@
 // Write your code below
+function isPalindrome(word) {
+    let j = word.length - 1
+    for (let i = 0; i < (word.length - 1) / 2; i++){
+        if (word[i] !== word[j]) {
+            return false
+        }
+        j --
+}
+return true
+}
+
+let palenResult = isPalindrome("moon")
+// isPalindrome("racecar")
+// isPalindrome("Racecar")
+
+console.log(palenResult)
